f78840dc22d15c456ddb3fa2afb19387273326c1
Michael Sealand <msealand@gmail.com>
2014-02-18 12:16:11 -0800

    Finish tests for plain mechanism
    PlainMechanism test coverage at 100%

--- test/mechanisms/plain.js
+++ test/mechanisms/plain.js
@@ -4,4 +4,39 @@ var should = require('should'),
 
 module.exports = function() {
 	util.validateMechanism(zmqzap.PlainMechanism);
+	it('should invalidate a message with no credentials', function() {
+		(function() {
+			var mechanism = new zmqzap.PlainMechanism(util.noCallAuthenticator);
+			mechanism.validate({}).should.not.be.ok;
+		}).should.not.throw();
+	});
+	it("should invalidate a message with credentials who's length is not 2", function() {
+		(function() {
+			var mechanism = new zmqzap.PlainMechanism(util.noCallAuthenticator);
+			mechanism.validate({credentials: []}).should.not.be.ok;
+		}).should.not.throw();
+	});
+	it("should validate a message with credentials of length 2", function() {
+		(function() {
+			var mechanism = new zmqzap.PlainMechanism(util.noCallAuthenticator);
+			mechanism.validate({credentials: [1, 1]}).should.be.ok;
+		}).should.not.throw();
+	});
+	it('should pass domain, address, username, and password to the authenticator', function(done) {
+		(function() {
+			var mechanism = new zmqzap.PlainMechanism(function(data, callback) {
+				should.exist(data);
+				should.exist(data.domain);
+				data.domain.should.eql("abc")
+				should.exist(data.address);
+				data.address.should.eql("127.0.0.1");
+				should.exist(data.username);
+				data.username.should.eql("user");
+				should.exist(data.password);
+				data.password.should.eql("pass");
+				done();
+			});
+			mechanism.authenticate({domain: "abc", address: "127.0.0.1", credentials: [new Buffer("user", "utf8"), new Buffer("pass", "utf8")]});
+		}).should.not.throw();
+	});
 }
